b3c8992c04000a2c5ffe4a19eb34503b3e88758e
Vincent <asd1111zxcv@gmail.com>
2023-04-26 14:26:26 +0800

    change

--- api.py
+++ api.py
@@ -3,7 +3,6 @@ import requests
 
 
 app = Flask(__name__)
-
 url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-093?Authorization=CWB-1E39B8CB-0ED8-427C-9921-DADF005A3BAC&locationId=F-D0047-003'
 params = {
     'Authorization': 'CWB-1E39B8CB-0ED8-427C-9921-DADF005A3BAC'
@@ -24,17 +23,32 @@ try :
         place = detail['locationName']
         start_time = detail['weatherElement'][0]['time'][0]['startTime']
         probility = detail['weatherElement'][0]['time'][0]['elementValue'][0]['value']
+        temperature = detail['weatherElement'][1]['time'][0]['elementValue'][0]['value']
+        wind = detail['weatherElement'][4]['time'][0]['elementValue'][0]['value']
+        
+        
+        info[place] = {'start_time':start_time ,
+                       'probility':probility,
+                      'temperature':temperature,
+                      'wind':wind
+                      }
 
         
-        print(place,start_time,probility)
         
-except  Exception as e:
+        
+except Exception as e :
     print(e)
 
 
+
 @app.route('/')
-def hello():
-    return jsonify({'message': probility})
+def ans():
+    # res = {
+    #     'place':place , 
+    #     'start_time':start_time,
+    #     'probility':probility
+    # }
+    return jsonify(info)
 
 if __name__ == '__main__':
     app.run()
